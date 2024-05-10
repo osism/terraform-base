29c5ef6103c7e95f0c4f13fc6ad19d18cb025539
Christian Berendt <berendt@osism.tech>
2024-05-10 15:05:33 +0200

    testbed-default: prepare use of a external API endpoint (#48)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ testbed-default/customisations/external_api_custom.tf
@@ -0,0 +1,19 @@
+resource "openstack_networking_secgroup_rule_v2" "security_group_management_rule_external_api_1" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  remote_ip_prefix  = "0.0.0.0/0"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  security_group_id = openstack_networking_secgroup_v2.security_group_management.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "security_group_management_rule_external_api_2" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  remote_ip_prefix  = "0.0.0.0/0"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  security_group_id = openstack_networking_secgroup_v2.security_group_management.id
+}

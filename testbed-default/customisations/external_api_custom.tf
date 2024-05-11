ccc95147f5c3f1873c135dcf82e0ea42af262afc
Christian Berendt <berendt@osism.tech>
2024-05-11 22:08:05 +0200

    testbed-default: add port 5000 to the external api (#50)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- testbed-default/customisations/external_api_custom.tf
+++ testbed-default/customisations/external_api_custom.tf
@@ -17,3 +17,13 @@ resource "openstack_networking_secgroup_rule_v2" "security_group_management_rule
   port_range_max    = 443
   security_group_id = openstack_networking_secgroup_v2.security_group_management.id
 }
+
+resource "openstack_networking_secgroup_rule_v2" "security_group_management_rule_external_api_3" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  remote_ip_prefix  = "0.0.0.0/0"
+  protocol          = "tcp"
+  port_range_min    = 5000
+  port_range_max    = 5000
+  security_group_id = openstack_networking_secgroup_v2.security_group_management.id
+}

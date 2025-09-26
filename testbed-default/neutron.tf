cb1f30da62be45dde704de9aaea656699afa0baa
Christian Berendt <berendt@osism.tech>
2025-09-26 23:06:30 +0200

    testbed-default: remove allocation pool (#90)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- testbed-default/neutron.tf
+++ testbed-default/neutron.tf
@@ -107,9 +107,4 @@ resource "openstack_networking_subnet_v2" "subnet_management" {
   cidr            = "192.168.16.0/20"
   ip_version      = 4
   dns_nameservers = var.dns_nameservers
-
-  allocation_pool {
-    start = "192.168.31.100"
-    end   = "192.168.31.200"
-  }
 }
